0c40908173626864db6c9f06156e7b88ae6da0b8
ignouuser <148249542+ignouuser@users.noreply.github.com>
2023-11-10 14:50:14 +0530

    Add files via upload

---
+++ Teams_202311101435.sql
@@ -0,0 +1,11 @@
+INSERT INTO TechnoCric24.dbo.Teams (TeamName,TeamCaptain,Player1,Player2,Player3) VALUES
+	 (N'Gabbars',N'Vinay M',N'Mrityunjay S',N'',N''),
+	 (N'Bhallaldevas',N'Biswajit G',N'Manashi A',N'',N''),
+	 (N'Ramadhirs',N'Rahul V',N'Shainu S',N'',N''),
+	 (N'Durjansinghs',N'T Bhaskar',N'Pinder M',N'',N''),
+	 (N'Kancha Cheenas',N'Debaprasad C',N'Srinivas D',N'',N''),
+	 (N'Mogambos',N'Atanu P',N'Vipul K',N'',N''),
+	 (N'Dr Dangs',N'Anup K',N'Uday G',N'',N''),
+	 (N'Shakaals',N'Avtar S',N'Saurabh H',N'',N''),
+	 (N'Teja Tejas',N'Siddhartha M',N'Abhishek P S',N'',N''),
+	 (N'Crime Master Gogos',N'Sudipto S',N'Balamurugan S',N'',N'');
